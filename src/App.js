b42a0151b59e39f9376cebff31477d80ac6a8ff5
Ivan Nikitin <iva3nik@yandex.ru>
2022-06-17 12:42:06 +0300

    feat: prepare the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import s from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={s.app}>
+      <h1 className={s.app__title}>Ты сегодня покормил кота?</h1>
+    </div>
+  );
+}
+
+export default App;
