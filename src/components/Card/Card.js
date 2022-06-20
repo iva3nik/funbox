c52f73a3592b87c90cdb381ddf13a70f7d18ea23
Ivan Nikitin <iva3nik@yandex.ru>
2022-06-20 14:30:03 +0300

    feat: add Choose cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import cn from "classnames";
 
 import s from "./Card.module.scss";
 
 const Card = ({ card }) => {
+  const [chosen, setChosen] = useState(false);
+
+  const handleChosenState = () => {
+    card.amount && setChosen(!chosen);
+  };
+
+  const handleText = () => {
+    return chosen ? (
+      <p className={s.card__buy}>{card.chosen}</p>
+    ) : (
+      <p className={s.card__buy}>
+        Чего сидишь? Порадуй котэ,{" "}
+        <span className={s.card__dashed} onClick={handleChosenState}>
+          купи
+        </span>
+        <span>.</span>
+      </p>
+    );
+  };
+
   return (
     <div className={s.card}>
-      <div className={cn(s.card__main, s.pentagon)}>
+      <div
+        className={cn(s.card__main, s.pentagon, {
+          [s.card__main_type_disabled]: card.amount <= 0,
+          [s.card__main_type_chosen]: chosen && card.amount,
+        })}
+        onClick={handleChosenState}
+      >
         <div className={cn(s.card__content, s.pentagon)}>
           <div>
             <p className={s.card__text}>Сказочное заморское яство</p>
@@ -26,10 +52,11 @@ const Card = ({ card }) => {
           </div>
         </div>
       </div>
-      <p className={s.card__buy}>
-        Чего сидишь? Порадуй котэ, <span className={s.card__dashed}>купи</span>
-        <span>.</span>
-      </p>
+      {card.amount ? (
+        handleText()
+      ) : (
+        <p className={cn(s.card__buy, s.card__buy_type_empty)}>{card.empty}</p>
+      )}
     </div>
   );
 };
